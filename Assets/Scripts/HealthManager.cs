0b7840f0b5e7805c93d56d0a5a6d8592c26d95ac
Kenny Park <parkkennypark@gmail.com>
2022-01-30 11:58:50 -0500

    Added sounds, fixed gun raycasting

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,15 +6,19 @@ public class HealthManager : MonoBehaviour
 {
     public int health;
 
-    public HealthManager(int health) {
+
+    public HealthManager(int health)
+    {
         this.health = health;
     }
 
-    public int GetHealth() {
+    public int GetHealth()
+    {
         return health;
     }
 
-    public void takeDamage(int damage) {
+    public void takeDamage(int damage)
+    {
         health = health - damage;
         /*if (health <= 0) {
            gameOver(); 
