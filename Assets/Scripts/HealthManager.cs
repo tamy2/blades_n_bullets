ae74cee892d100e75546116e2c317ee200a2b9e6
tamy2 <tamy@purdue.edu>
2022-01-30 11:58:37 -0500

    Health Manager/Bar Updates

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    public int health;
+    public int fullHealth;
+    private int currentHealth;
+    public HealthBar bar;
 
+    /*
     public HealthManager(int health) {
-        this.health = health;
+        this.health = currentHealth;
+    }
+    */
+
+    private void Start() {
+        currentHealth = fullHealth;
     }
 
     public int GetHealth() {
-        return health;
+        return currentHealth;
     }
 
     public void takeDamage(int damage) {
-        health = health - damage;
+        currentHealth = currentHealth - damage;
         /*if (health <= 0) {
            gameOver(); 
         }*/
+        float ratio = (float) currentHealth/ (float) fullHealth;
+        bar.OnDamageTaken(ratio);
         print(damage + " damage taken!");
+        print("current total health: " + currentHealth);
+        print("Full health: " + fullHealth);
+        print("current health ratio: " + currentHealth/fullHealth);
     }
 }
